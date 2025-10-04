3786297601083bfe4470fb4fd06f8017bd6eb6ce
Shivnath Gupta <codexshiv@gmail.com>
2025-10-04 23:58:26 +0530

    today work day2

---
+++ Madlibsgame.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+  //mad libs game
+  char noun[50] = "";
+  char verb[50] = " ";
+  char adjective1[50] = "";
+  char adjective2[50] = "";
+  char adjective3[50] = "";
+
+  printf("Enter an adjective (description):");
+  fgets(adjective1, sizeof(adjective1), stdin);
+
+  printf("Enter a noun (animal or person):");
+  fgets(noun, sizeof(noun), stdin);
+
+  printf("Enter an adjective (description):");
+  fgets(adjective2,sizeof(adjective2),stdin);
+
+
+  printf("Enter a verb (ending w/ -ing):");
+  fgets(verb,sizeof(verb) , stdin);
+
+  printf("Enter an adjective (description):");
+  fgets(adjective3,sizeof(adjective3),stdin);
+  
+
+   Printf("\nToday I went to a %s Zoo.\n",adjective1);
+   Printf("In an exhibit, I saw a %s. \n",noun);
+   Printf("%s was %s and %s!\n",noun,adjective2,verb);
+   printf("I was %s!\n", adjective3);
+
+   return 0;
+  
+}
