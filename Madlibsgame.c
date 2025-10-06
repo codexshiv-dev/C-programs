894b3433ad9e46f634e27de9cc5ce4ad1a5a71bc
Shivnath Gupta <codexshiv@gmail.com>
2025-10-06 23:53:36 +0530

    day3 task

--- Madlibsgame.c
+++ Madlibsgame.c
@@ -11,6 +11,7 @@ int main(){
   printf("Enter an adjective (description):");
   fgets(adjective1, sizeof(adjective1), stdin);
   adjective1[strlen(adjective1)- 1] = '\0';
+
   printf("Enter a noun (animal or person):");
   fgets(noun, sizeof(noun), stdin);
    noun[strlen(noun)- 1] = '\0';
@@ -22,11 +23,12 @@ int main(){
   printf("Enter a verb (ending w/ -ing):");
   fgets(verb,sizeof(verb) , stdin);
  verb[strlen(verb)- 1] = '\0';
+
   printf("Enter an adjective (description):");
   fgets(adjective3,sizeof(adjective3),stdin);
    adjective3[strlen(adjective3)- 1] = '\0';
 
-   Printf("\nToday I went to a %s Zoo.\n",adjective1);
+   Printf("\n Today I went to a %s Zoo.\n",adjective1);
    Printf("In an exhibit, I saw a %s. \n",noun);
    Printf("%s was %s and %s!\n",noun,adjective2,verb);
    printf("I was %s!\n", adjective3);
